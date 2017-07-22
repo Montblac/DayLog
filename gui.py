0ec33f7748e3e56d78ec9034cb8ef825c4961333
Montblac <montblac95@gmail.com>
2017-07-21 18:33:19 -0700

    - Fixed the imports - Fixed get() method - Implemented add(): now properly creates/opens files and appends input - Implemented update(): clears the entry box after each use - Implemented show(): prints the logs for the day with timestamps

--- gui.py
+++ gui.py
@@ -1,9 +1,6 @@
-from tkinter import Tk, mainloop, Label, Entry
-from time import strftime
+from tkinter import Tk, Label, Entry
+from time import strftime, localtime
 from os import getcwd, path
-
-# Remove after
-from os import listdir
 
 class AppGUI:
     """ Creates the gui for the daylog
@@ -22,28 +19,26 @@ class AppGUI:
         self.entry.grid(row=0, column=1)
         self.entry.configure(bg=self.bg, fg=self.fg, highlightbackground=self.bg)
         self.entry.focus_set()
-
         self.entry.bind('<Return>', self.add)
 
 
-    def get(self, event):
+    def get(self):
         """ Returns the text entry of the user
         """
-        return event.widget.get()
+        return self.entry.get()
 
     def add(self, event):
         """ Adds user input to text file
         """
-        # Current date in format month-day-year with leading zeros
-        file = strftime("%m-%d-%Y")
-        current_dir = getcwd()
-        result = current_dir + '\\' + file
+        base = getcwd()
+        filename = strftime("%Y-%d-%m") + ".txt"
+        filepath = path.join(base, filename)
 
-        print(listdir(current_dir))
-        print(path.isfile(result))
-        print(getcwd())
-        print(result)
+        with open(filepath, 'a+') as fp:
+            fp.write("[{}]\t{}".format(strftime("%H:%M:%S", localtime()), self.get()))
 
+        self.update()
+        self.show()
 
     def remove(self):
         """ Removes user input from text file
@@ -53,7 +48,18 @@ class AppGUI:
     def update(self):
         """ Updates window with a clean entry box
         """
-        pass
+        self.entry.delete(0, 'end')
+
+    def show(self):
+        """ Prints the logs for the day
+        """
+        base = getcwd()
+        filename = strftime("%m-%d-%Y")
+        filepath = path.join(base, filename)
+
+        with open(filepath, 'r') as fp:
+            for line in fp:
+                print(line)
 
     def start(self):
         """ Starts the GUI window
