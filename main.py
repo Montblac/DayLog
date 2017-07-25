bb41bcb83cd9fe47fc9a5d473f66ab35d513b884
Montblac <montblac95@gmail.com>
2017-07-25 16:39:55 -0700

    - Removed some extra notes.

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ if __name__ == '__main__':
     new_gui = AppGUI()
     new_gui.start()
 
-    # Launch user gui
-    # Take user input
     # Write user input to file (or store to db)
     # Use format (Mon-Day-Year Time input)
